9673068d066c92ccfc7b750fd12d88621a1d33e7
evilevgeniys <903jenya@gmail.com>
2023-11-16 21:50:28 +0300

    intermediate commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const box = document.querySelector('.box');
+const btn = document.querySelector('button');
+
+const width = box.clientWidth;
+const height = box.clientHeight;
+
+btn.addEventListener('click', () => {
+    box.style.height = box.scrollHeight + 'px';
+})
+
+console.log(width, height);
