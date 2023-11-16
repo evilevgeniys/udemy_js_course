8bf94f2cf6f8c58430717a3e90e772b826650814
evilevgeniys <903jenya@gmail.com>
2023-11-16 12:00:40 +0300

    Added check for past date

--- src/js/main.js
+++ src/js/main.js
@@ -43,14 +43,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //Timer 
 
-    const deadline = '2023-12-16';
+    const deadline = '2023-11-17';
 
     function getTimeRemaining(endtime){
         const t = Date.parse(endtime) - Date.parse(new Date());
-        const days = Math.floor(t / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-        const minutes = Math.floor((t / (1000 * 60)) % 60 );
-        const seconds = Math.floor((t / 1000) % 60);
+        let days, hours, minutes, seconds;
+
+        if (t <= 0){
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24));
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+            minutes = Math.floor((t / (1000 * 60)) % 60 );
+            seconds = Math.floor((t / 1000) % 60);
+        }
 
         return {
             'total': t,
