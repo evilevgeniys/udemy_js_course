4a581d96f93877338933b8218ac8017828b2ce90
evilevgeniys <903jenya@gmail.com>
2023-11-16 11:48:10 +0300

    add timer

--- src/js/main.js
+++ src/js/main.js
@@ -40,5 +40,60 @@ window.addEventListener('DOMContentLoaded', () => {
             })
         }
     })
+
+    //Timer 
+
+    const deadline = '2023-12-16';
+
+    function getTimeRemaining(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        const days = Math.floor(t / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((t / (1000 * 60)) % 60 );
+        const seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    };
+
+    function getZero(num){
+        if (num >= 0 && num < 10){
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endTime){
+        const timer = document.querySelector(selector);
+        const days = timer.querySelector('#days');
+        const hours = timer.querySelector('#hours');
+        const minutes = timer.querySelector('#minutes');
+        const seconds = timer.querySelector('#seconds');
+        
+        updateClock();  
+        
+        timeInterval = setInterval(updateClock, 1000);
+
+    }
     
+    function updateClock(){
+        const t = getTimeRemaining(deadline);
+
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if(t.total <= 0){
+            clearInterval(timeInterval);
+        } 
+    }
+
+    setClock('.timer', deadline);
 })
