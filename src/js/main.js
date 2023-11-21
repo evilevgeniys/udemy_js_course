263b81bcd26d3df9211f817e812fd0ed4616a130
evilevgeniys <903jenya@gmail.com>
2023-11-21 21:32:31 +0300

    fixed bag

--- src/js/main.js
+++ src/js/main.js
@@ -179,12 +179,12 @@ window.addEventListener('DOMContentLoaded', () => {
             element.innerHTML = `
                 <div class="menu__item">
                     <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle>${this.title}</h3>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-divider"></div>
                     <div class="menu__item-descr">${this.descr}</div>
                     <div class="menu__item-price">
                         <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> BYN/день</div>
+                        <div class="menu__item-total"><span>${this.price}</span>BYN/день</div>
                     </div>
                 </div>
             `;
