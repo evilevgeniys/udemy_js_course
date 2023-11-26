0d345faf033dbad36889c4032a88e875170aba49
evilevgeniys <903jenya@gmail.com>
2023-11-26 17:57:57 +0300

    added conditions for chrecking the parameter array for emptiness

--- src/js/main.js
+++ src/js/main.js
@@ -177,7 +177,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
-            this.classes.forEach(className => element.classList.add(className))
+
+            if(this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className))
+            }
+            
             element.innerHTML = `
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
