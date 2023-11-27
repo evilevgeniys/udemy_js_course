643e2a78fe619ef73bddae4bdb276701af4305f7
evilevgeniys <903jenya@gmail.com>
2023-11-27 19:34:10 +0300

    swap readystatechange to load

--- src/js/script.js
+++ src/js/script.js
@@ -12,11 +12,10 @@ inputRub.addEventListener('input', () => {
     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     request.send();
 
-    request.addEventListener('readystatechange', () => {
-        if(request.readyState === 4 && request.status === 200){
-            console.log(request.response);
+    request.addEventListener('load', () => {
+        if(request.status === 200){
             const date = JSON.parse(request.response);
-            inputUsd.value = +inputRub.value / date.current.usd;
+            inputUsd.value = (+inputRub.value / date.current.usd).toFixed(2);
         } else {
             inputUsd.value = "Что-то пошло не так";
         }
